1f1b96bfbe53c9491958764f2c5071019a8755ae
Diego Flores <del.angelfull@gmail.com>
2022-03-10 16:10:31 -0600

    Updates

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 //=======> nav and menu
 const  menu = document.getElementById("menu"),
     btnMenu=document.querySelector("#btn-menu"),
-    icon = document.querySelector('.icon'),
-    subm = document.getElementById("subm");
+    icon = document.querySelector('.icon');
 
-var nav = document.querySelector(".nav");   
+var nav = document.querySelector(".nav"); 
+let subm=  document.getElementById("subm");
+
 
 btnMenu.addEventListener('click', ()=>{   //Mostrar menu
         menu.classList.toggle('show-menu');
         icon.classList.toggle('uil-apps'); //Cambio de iconos 
         icon.classList.toggle('uil-times');
+        document.getElementById("screwic").classList.toggle('show-sc');
         document.body.classList.toggle('no-scroll'); //Desactiva el sroll al dar click
 
+        if(menu.classList.contains('show-menu')){ //Pregunta si tiene esa clase 
+            subm.textContent="Servicios";
+        }else{
+            subm.textContent="Celularreglo";
+        }
 })
 
 
